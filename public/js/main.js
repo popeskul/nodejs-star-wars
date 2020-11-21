fb93a0a86b6a9082dabf1e47ef392b15a133e1d1
Pavel Popeskul <peterjj91@gmail.com>
2020-11-21 16:59:57 +0200

    Fetch a lot of persons

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
-var selectFilm = document.querySelector('.select-film');
+const selectFilm = document.querySelector('.select-film');
+const personageLoader = document.querySelector('.personage-loader');
+const personageResults = document.querySelector('.personage-results');
 if (selectFilm) {
   selectFilm.addEventListener('change', function (event) {
+    personageLoader.style.display = 'block';
+    personageResults.style.display = 'none';
     document.location.replace(`/${selectFilm.value}`);
   });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const { throwError, forkJoin } = require('rxjs');
-const { catchError } = require('rxjs/operators');
+const {
+  catchError,
+  mergeMap,
+  tap,
+  switchMap,
+  combineLatest,
+  map
+} = require('rxjs/operators');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
@@ -16,20 +23,33 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/:id?', function (req, res, next) {
+router.get('/:id', function (req, res, next) {
+  // fetch data for options and we fetch film by params.id
   const results = forkJoin({
     film: axios(`https://swapi.dev/api/films/${req.params.id}`),
     filmList: axios(`https://swapi.dev/api/films/`)
   });
 
+  // handle errors
   results.pipe(catchError((error) => throwError('Bad url', error.config.url)));
+
   results.subscribe(
     ({ filmList, film }) => {
-      console.log(film.data);
-      res.render('index', {
-        filmList: filmList.data.results,
-        film: film.data,
-        currentValue: Number(req.params.id) + 1
+      // now we need to fetch a lot of persons
+      const peopleResults = forkJoin({
+        ...film.data.characters.map((char) => axios(char))
+      });
+
+      peopleResults.subscribe((i) => {
+        // then we need to take just names
+        const names = Object.values(i).map((a) => a.data?.name);
+
+        // final render
+        res.render('index', {
+          filmList: filmList.data.results,
+          names,
+          currentValue: Number(req.params.id) + 1
+        });
       });
     },
     (error) => res.render('error', { error })
